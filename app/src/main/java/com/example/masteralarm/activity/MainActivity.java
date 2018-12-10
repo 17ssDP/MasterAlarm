12e3a981858b54cbec733870f999c7590324dbf5
Jedar <384034509@qq.com>
2018-12-10 16:22:16 +0800

    #test

--- app/src/main/java/com/example/masteralarm/activity/MainActivity.java
+++ app/src/main/java/com/example/masteralarm/activity/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.widget.Toast.makeText("test", "main activity", Toast.LENGTH_SHORT).show();
     }
 }
