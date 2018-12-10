e01b046764320f9d4a89e0e9129bfc9697c19f6b
Your Name <17302010026@fudan.edu.cn>
2018-12-10 16:24:56 +0800

    test
    test

--- app/src/main/java/com/example/masteralarm/activity/MainActivity.java
+++ app/src/main/java/com/example/masteralarm/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.masteralarm.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.masteralarm.R;
 
@@ -11,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        android.widget.Toast.makeText("test", "main activity", Toast.LENGTH_SHORT).show();
+        //test
     }
 }
